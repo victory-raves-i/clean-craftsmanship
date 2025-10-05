24f14a1bb5b044238cd0c4c90756594cb18a7f3c
Manuel Fernandez <manfer.alonso@gmail.com>
2025-10-05 09:33:47 +0200

    test: pass but not having into consideration , encasulaption

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -12,10 +12,24 @@ func TestCreateEmptyQueue(t *testing.T) {
 	}
 }
 
-func TestEnqueue(t *testing.T) {
+func TestEnqueueOneItem(t *testing.T) {
 	q := Queue{}
-	q.Enqueue(1)
+	q.Enqueue(10)
 	if q.Size() != 1 {
-		t.Error("Somthing went wrong trying to enqueue and item")
+		t.Error("Something went wrong trying to enqueue and item")
+	}
+	if q.elements[q.Size()-1] != 10 {
+		t.Error("Queue its no able to store one value correctly")
+	}
+}
+
+func TestEnqueueMultipleItems(t *testing.T) {
+	q := Queue{}
+
+	q.Enqueue(1)
+	q.Enqueue(2)
+
+	if q.Size() != 2 {
+		t.Error("Enqueue multiple values is not working")
 	}
 }
