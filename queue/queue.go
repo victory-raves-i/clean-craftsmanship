4ae925d65c150fbcd20976bdde6aeeb3a7d6c4a4
Manuel Fernandez <manfer.alonso@gmail.com>
2025-10-05 09:14:21 +0200

    feat: add enqueue functionality

--- queue/queue.go
+++ queue/queue.go
@@ -1,7 +1,13 @@
 package queue
 
-type Queue struct{}
+type Queue struct {
+	elements []int
+}
 
-func (q Queue) Size() int {
-	return 0
+func (q *Queue) Size() int {
+	return len(q.elements)
+}
+
+func (q *Queue) Enqueue(value int) {
+	q.elements = append(q.elements, value)
 }
