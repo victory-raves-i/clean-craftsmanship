a9814d37c75e5a9e5a1219b5f8c679ddef2232d3
Manuel Fernandez <manfer.alonso@gmail.com>
2025-10-17 19:29:52 +0200

    tests: second degenerate test

--- word-wrap/word_wrap_test.go
+++ word-wrap/word_wrap_test.go
@@ -5,3 +5,11 @@ import "testing"
 func TestCreateEmptyTest(t *testing.T) {
 	t.Error("It Fails")
 }
+
+func TestEmptyString(t *testing.T) {
+	w := Wrap(text, width)
+
+	if w == nil {
+		t.Error("Function does not exist or it is not declared")
+	}
+}
