1b52180be547756c8a674d330a4216c954ee3e92
Manuel Fernandez <manfer.alonso@gmail.com>
2025-10-18 09:57:28 +0200

    tests: delete nothing test and add more basic test of wrapping

---
+++ wordwrap/wordwrap_test.go
@@ -0,0 +1,19 @@
+package wordwrap
+
+import "testing"
+
+func TestEmptyString(t *testing.T) {
+	w, err := Wrap("", 8)
+
+	if err != nil {
+		t.Error("Function does not exist or it is not declared")
+	}
+
+	if w != "" {
+		t.Error("Function does not work with empty string")
+	}
+}
+
+func TestTextSmallerThanWidth(t *testing.T) {
+	w, err := Wrap("the quick brown fox jumps", 100)
+}
