7c941760ed68f1f2b623d3abb74c320098bdf942
Manuel Fernandez <manfer.alonso@gmail.com>
2025-10-18 10:53:22 +0200

    feat: make TextBiggerThanWidth pass

--- wordwrap/wordwrap.go
+++ wordwrap/wordwrap.go
@@ -1,5 +1,17 @@
 package wordwrap
 
-func Wrap(text string, width int) (string, error) {
-	return text, nil
+func Wrap(text string, width int) ([]string, error) {
+	var s []string
+
+	if len(text) < width {
+		s = append(s, text)
+		return s, nil
+	} else {
+		s = append(s, text[:width])
+		s = append(s, text[width:])
+
+	}
+
+	return s, nil
+
 }
